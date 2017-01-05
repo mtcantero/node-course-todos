c760af907751f0de5dbfe62b570c76b794245e8f
mtcantero <mario.trueba@outlook.com>
2017-01-05 21:45:19 +0000

    First Commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,36 @@
+const {MongoClient,ObjectID} = require('mongodb');
+
+
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp' ,(error,db) =>{
+    if (error){
+        return console.log('Unable to connect to MongoDb Server');
+    }
+    console.log('Connected to MongoDb Server');
+
+    // db.collection('Todos').insertOne({
+    //     text: 'Something to do',
+    //     completed: false
+
+    // }, (error, result) =>{
+    //     if (error){
+    //         console.log('Unable to insert the record: ', error);
+    //     }
+
+    //     console.log(JSON.stringify(result.ops,undefined, 2))
+    // })
+
+    // db.collection('Users').insertOne({
+    //     name: 'Mario',
+    //     age: 28,
+    //     location: 'London,England'
+    // }, (error,result) =>{
+    //     if(error){
+    //         console.log('Unable to insert the record, because of: ', error);
+    //     }
+
+    //     console.log(result.ops[0]._id.getTimestamp());
+    // })
+
+    db.close();
+});
---
+++ playground/mongodb-find.js
@@ -0,0 +1,33 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp' ,(error,db) =>{
+    if (error){
+        return console.log('Unable to connect to MongoDb Server');
+    }
+    console.log('Connected to MongoDb Server');
+
+    // db.collection('Todos').find({
+    //     _id: new ObjectID('586eb850ef0ffbb6def9e631') }).toArray().then((docs) => {
+    //     console.log('Todos List');
+    //     console.log(JSON.stringify(docs,undefined,2));
+    // }, (err) => {
+    //     console.log('Error : ', err);
+    // });
+
+    // db.collection('Todos').find().count().then((count) => {
+    //     console.log('Todos count' + count);
+    //     //console.log(JSON.stringify(docs,undefined,2));
+    // }, (err) => {
+    //     console.log('Error : ', err);
+    // });
+
+    db.collection('Users').find({
+        name : 'Mario'
+    }).toArray().then((docs) => {
+        console.log(JSON.stringify(docs,undefined,2));
+    }, (error) =>{
+        console.log('Unable to find Users, ', error);
+    } );
+
+    //db.close();
+});
