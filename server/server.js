c9acd2e2f370d2ab280f3b48a8ac9ef92d04c31c
mtcantero <mario.trueba@outlook.com>
2017-01-07 19:11:35 +0000

    Add GET todos ID

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 
-
+var {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
@@ -28,8 +28,28 @@ app.get('/todos', (req,res) => {
     Todo.find().then((todos) => {
         res.send({todos});
     }, (e) => {
-        res.status(400).send(e);
+        res.status(404).send(e);
     })
+});
+
+app.get('/todos/:id', (req,res) => {
+
+    var id = req.params.id;
+    if (!ObjectID.isValid(id)){
+        res.status(400).send();
+    }
+
+    Todo.findById(id).then((todo) => {
+        if (!todo){
+            res.status(404).send();
+        }
+        res.send({todo});
+
+    }, (e) => {
+        res.status(400).send();
+    })
+
+
 });
 
 app.listen(3000, () =>{
